3446a5c4107378c03b33f3df76df578007462903
Elliot Voris <elliot@stellar.org>
2026-03-18 15:34:34 -0500

    add typing for preview object on landing page

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -8,13 +8,15 @@ import {
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = () => {
+    const previews: Record<string, Record<string, unknown>> = {
+        'space-weather': getSpaceWeatherPreview(),
+        'near-earth-objects': getNearEarthObjectsPreview(),
+        exoplanets: getExoplanetsPreview(),
+        'deep-sky-catalog': getDeepSkyCatalogPreview(),
+        'gravitational-waves': getGravitationalWavesPreview(),
+    };
+
     return {
-        previews: {
-            'space-weather': getSpaceWeatherPreview(),
-            'near-earth-objects': getNearEarthObjectsPreview(),
-            exoplanets: getExoplanetsPreview(),
-            'deep-sky-catalog': getDeepSkyCatalogPreview(),
-            'gravitational-waves': getGravitationalWavesPreview(),
-        },
+        previews,
     };
 };
